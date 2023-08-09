3bbbc16628ca3e7d6bd0170d7f742d2f535b34b0
Yang.YueBin <yangyuebin@cvte.com>
2023-08-09 10:47:15 +0800

    [improve] 移除冗余文件

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 打包上传
-./gradlew -p core clean assembleRelease || { exit 1 ; }
-./gradlew -p live clean assembleRelease || { exit 1 ; }
-./gradlew -p glide clean assembleRelease || { exit 1 ; }
